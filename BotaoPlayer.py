8d227b15aa0c945586f5acc2d15e49dd081bb67c
Rodrigo Evangelista <rodrigokamaroskievangelista@gmail.com>
2026-04-24 14:36:54 -0300

    alteracao funcao e remove = para .tittle

--- BotaoPlayer.py
+++ BotaoPlayer.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 
-janela = tk.Tk()
-janela = title("Player")
-janela.geometry("300x200")
-
-janela.mainloop()
-
 def play():
     print("Play")
 
-btn = tk.button(janela, text="Play", command=play)
+janela = tk.Tk()
+janela.title("Player")
+janela.geometry("300x200")
+
+btn = tk.Button(janela, text="Play", command=play)
 btn.pack()
+
 janela.mainloop()
